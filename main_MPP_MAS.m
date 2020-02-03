1c4545ab7a8d56ddbba9d52ac97247fdedd41009
HUOJIAXI <JasonHuoeclille@gmail.com>
2020-02-03 18:03:21 +0100

    3-MAS

--- main_MPP_MAS.m
+++ main_MPP_MAS.m
@@ -6,9 +6,9 @@ clear;
 clc;
 D = load('tsp_map.txt'); 
 temp = D;
-Start = [2,8];
-Goal = [143,96];
-RobotNum = 2;
+Start = [2,8,49];
+Goal = [143,96,137];
+RobotNum = 3;
 %% 求解
 tic
 PathStore=MASPP_IP(D,RobotNum,Start,Goal);
