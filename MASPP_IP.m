4ec24004e001f9971fbf654c74698f9f3bae1e51
HUOJIAXI <JasonHuoeclille@gmail.com>
2020-02-03 17:53:57 +0100

    Sequential MAS

--- MASPP_IP.m
+++ MASPP_IP.m
@@ -3,7 +3,17 @@ temp = D;
 PathStore=cell(RobotNum,1);
 
 for i = 1:RobotNum
-    [PATH,temp]=IP_solver(temp,Start,Goal);
+    [PATH,temp]=IP_solver(temp,Start(i),Goal(i),i);
     PathStore{i} = PATH;
 end
+
+mapdesigner(fliplr(D));
+hold on;
+
+for i = 1:RobotNum
+    plot((PathStore{i}(:,2)-1/2),(PathStore{i}(:,1)-1/2),'-ks','MarkerFaceColor','r','MarkerSize',10)
+    hold on;
+end
+
+
 
--- main_MPP_MAS.m
+++ main_MPP_MAS.m
@@ -6,9 +6,9 @@ clear;
 clc;
 D = load('tsp_map.txt'); 
 temp = D;
-Start = 2;
-Goal = 143;
-RobotNum = 1;
+Start = [2,8];
+Goal = [143,96];
+RobotNum = 2;
 %% 求解
 tic
 PathStore=MASPP_IP(D,RobotNum,Start,Goal);
