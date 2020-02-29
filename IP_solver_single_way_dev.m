313b80931e09b8914c3e3034cde9c39f1a07f7bb
HUOJIAXI <JasonHuoeclille@gmail.com>
2020-02-29 20:16:58 +0100

    Version one way 1.1

--- IP_solver_single_way_dev.m
+++ IP_solver_single_way_dev.m
@@ -2,7 +2,7 @@
 % Author:HUO JIAXI
 % 引入单行道限制
 %% 待修改
-function [PATH,Path]=IP_solver_single_way(D,l,t,numrobot,size_D)
+function [PATH,Path]=IP_solver_single_way_dev(D,l,t,numrobot,size_D)
 PATH=cell(numrobot,1);
 Path=cell(numrobot,1);
 o_single=cell(numrobot,1);
@@ -11,15 +11,12 @@ d = transfer(D);
 n = size(d,1); 
 % 决策变量
 
-x = binvar(numrobot,n,n,'full'); % n*n维的决策变量
+x = binvar(n,n,numrobot,'full'); % n*n维的决策变量
 u = intvar(numrobot,n);
-dir = intvar(numrobot,n,n,'full'); % a 表示每个巷道的方向
+dir = intvar(n,n,numrobot,'full'); % a 表示每个巷道的方向
 
 % 目标
-z=0;
-for i = 1:numrobot
-    z = z+sum(sum(d.*x(i,:,:)));
-end
+z = sum(sum(d.*x(:,:,1)));
 
 % 约束添加
 C = [];
@@ -33,15 +30,16 @@ if l == t
     disp('起点终点在同一个点')
     return
 end
+
 %% 约束1 确保路径从起点出发并在终点结束
 for i = 1:numrobot
-    C = [C, sum(x(i,l(i),:)) - x(i,l(i),l(i)) - sum(x(i,:,t(i))) + x(i,t(i),t(i))== 0];
+    C = [C, sum(x(l(i),:,i))-x(l(i),l,i)-sum(x(:,t,i))+x(t,t,i)==0];
 
-    C = [C, sum(x(i,l(i),:)) - x(i,l(i),l(i)) == 1];
+    C = [C, sum(x(l,:,i)) - x(l,l,i) == 1];
 
-    C = [C, sum(x(i,:,l(i))) - x(i,l(i),l(i)) - sum(x(i,t(i),:)) + x(i,t(i),t(i)) == 0];
+    C = [C, sum(x(:,l,i)) - x(l,l,i) - sum(x(t,:,i)) + x(t,t,i) == 0];
 
-    C = [C, sum(x(i,:,l(i))) - x(i,l(i),l(i)) == 0]; 
+    C = [C, sum(x(:,l,i)) - x(l,l,i) == 0]; 
 end
 %% 约束2 确保出入边条件，每个顶点在路径中仅出现一次
 for k=1:numrobot
@@ -57,14 +55,14 @@ end
 for k=1:numrobot
     for i = 1:n
         for j = 1:n
-            if i~=j && i ~=l(k) && i ~=t(k) && j ~=l(k) && j ~=t(k)
+            if i~=j && i ~=l && i ~=t && j ~=l && j ~=t
                 C = [C,u(k,i)-u(k,j) + (n-3)*x(k,i,j)<=n-4];
             end
         end
     end
 end
 
-%% 约束4 单行线法则（交叉点不可只出不进或只进不出）
+%% 约束4 单行线法则
 for k = 1:numrobot
     for i = 1:n
        for j = 1:n
@@ -90,13 +88,6 @@ for k = 1:numrobot
         end
     end
 end
-%% 约束5 单行线法则 （巷道方向框定）
-
-for i = 1:n
-    for j = 1:n
-        C = [C, dir(1,1,2)-dir(2,3,2)+dir(2,8,7)+dir(1,7,8)==0]; %% 仅对于3*3小环境有效
-    end
-end
 
 %% 
 %% 求解IP模型
@@ -110,15 +101,15 @@ result  = optimize(C,z,ops);
 if result.problem== 0
 %    value(x)
 %    disp(value(z))
-    text=' 系统总最优路径长度：';
-    disp([text,num2str(value(z))]);
+    text=' 号机器人最优路径求解成功，最优路径长度：';
+    disp([num2str(numrobot),text,num2str(value(z))]);
 else
     disp('求解路径中存在障碍物，起点终点无法直达');
 end
 
-%disp(value(dir))
+disp(value(dir))
 o=value(x);
-
+%disp(o);
 for k=1:numrobot
     for i=1:length(o)
         for j = 1 : length(o)
@@ -130,18 +121,16 @@ for k=1:numrobot
         end
     end
 end
-
-%disp(o(2,:,:));
 %% 邻接矩阵转换，路径绘制
 
 for i = 1:numrobot
-    o_single{i,1} =  squeeze(o(i,:,:)) ; 
+    o_single{i,1} = reshape(o, n,n*i); 
 end
 
 for i = 1:numrobot
     o_sin=o_single{i,1};
-%    disp(o_sin)
-    Path{i,1}=solvermatrix(o_sin,l(i),t(i));
+    disp(o_sin)
+    Path{i,1}=solvermatrix(o_sin,l,t);
     m = size(D,1);
     [X,Y]=spread(Path{i,1},m);
     PATH{i,1}=cat(1,X,Y)'; % 路径存入PATH matrix
