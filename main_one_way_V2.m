ec0f8269c29a43c3d3f7a0400a93fc1f0311ab8b
HUOJIAXI <JasonHuoeclille@gmail.com>
2020-03-20 21:03:58 +0100

    file split

--- main_one_way_V2.m
+++ main_one_way_V2.m
@@ -7,50 +7,13 @@ RobotNum=4;
 %RobotNum = size(Start,2);
 
 [Start,Goal,start_sp,goal_sp,D_reduit] = reduit(Start_ori,Goal_ori,D);
-% disp(D_reduit)
-% disp(Start)
-% disp(Goal)
 size_D=size(D_reduit,2);
-% size_D=size(D,2);
 
 diary('res_16.txt');
 
-test = cat(2,Start',Goal');
-equal=[];
-Start_new=[];
-Goal_new=[];
-equal_now=[];
-for i = 1:RobotNum-1
-    test_o=test(i,:);
-    if ~ismember(i,equal)
-        for j = i+1:RobotNum
+disp(datestr(now));
 
-                test_o_i=test(j,:);
-                if (test_o-test_o_i)==0
-
-                    equal=[equal j];
-
-                end
-        end
-    end
-end
-
-for j =1:RobotNum
-
-    if ~ismember(j,equal)
-        Start_new = [Start_new Start(j)];
-        Goal_new  = [Goal_new Goal(j)];
-    end
-
-end
-
-RobotNum_new=length(Start_new);
-
-if length(Start_new)-length(Start)==0
-    disp('不存在重合起点终点');
-else
-    disp('存在重合起点终点');
-end
+[Start_new,Goal_new,RobotNum_new]=test_reduce_coincidence(Start,Goal,RobotNum);
 
 tic
  [PathStore,Path_num]=IP_solver_single_way_V2(D_reduit,Start_new,Goal_new,RobotNum_new,size_D);
@@ -58,27 +21,7 @@ tic
 toc
 diary('off');
 
-PathStore_re=cell(RobotNum,1);
-Path_num_re=cell(RobotNum,1);
-
-for i = 1:RobotNum
-    start_find = find(Start_new==Start(i));
-    goal_find = find(Goal_new==Goal(i));
-    
-    for j = 1:length(start_find)
-        for m = 1:length(goal_find)
-            if start_find(j)==goal_find(m)
-                findindex=start_find(j);
-            end
-        end
-    end
- 
-       PathStore_re{i,1}=PathStore{findindex,1};
-       Path_num_re{i,1}=Path_num{findindex,1};
-    
-end
-PathStore=PathStore_re;
-Path_num=Path_num_re;
+[PathStore,Path_num] = rebuild_path(RobotNum,Start,Goal,Start_new,Goal_new,PathStore,Path_num);
 
 [PathStore_new,Path_num_new]=broaden(PathStore,D,RobotNum,Start_ori,Goal_ori);
 
