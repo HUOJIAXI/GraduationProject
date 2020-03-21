3deae21bf4874656c905e8ca264bccec835efc5a
HUOJIAXI <JasonHuoeclille@gmail.com>
2020-03-21 20:40:38 +0100

    version test

--- IP_solver_single_way_test.m
+++ IP_solver_single_way_test.m
@@ -1,4 +1,4 @@
-function [PATH,Path,dir_way_value,dir_rob_value]=IP_solver_single_way_test(D,l,t,numrobot,size_D,ini_dir_way,ini_dir_rob)
+function [PATH,Path,dir_way_value,dir_rob_value,x_value,u_value]=IP_solver_single_way_test(D,l,t,numrobot,size_D,ini_dir_way,ini_dir_rob,ini_x_value,ini_u_value)
 PATH=cell(numrobot,1);
 Path=cell(numrobot,1);
 o_single=cell(numrobot,1);
@@ -7,7 +7,7 @@ timelimit=round(50*numrobot);
 
 m_D=size(D,1);
 n_D=size(D,2);
-num_way=((m_D+1)*(n_D-1)+(m_D-1)*(n_D+1))/4; % 需要利用节点方向推出边占用情况
+ num_way=((m_D+1)*(n_D-1)+(m_D-1)*(n_D+1))/4; % 需要利用节点方向推出边占用情况
 
 d = transfer(D);
 m = size(d,1); 
@@ -26,8 +26,8 @@ for i = 1:m_D
     end
 end
 
-x = binvar(numrobot,n,n,'full'); % n*n维的决策变量
-u = intvar(numrobot,n,1);
+x = binvar(n,n,numrobot,'full'); % n*n维的决策变量
+u = intvar(numrobot,n,'full');
 % dir = intvar(numrobot,n,n,'full'); % a 表示每个机器人的方向
 dir_way=intvar(1,num_way,'full');
 dir_rob=intvar(numrobot,num_way,'full');
@@ -39,7 +39,7 @@ dir_rob=intvar(numrobot,num_way,'full');
 
 z=0;
 for i = 1:numrobot
-    z = z+sum(sum(d.*x(i,:,:)));
+    z = z+sum(sum(d.*x(:,:,i)));
 end
 % 
 % for i = 1:numrobot
@@ -62,21 +62,21 @@ if l == t
 end
 %% 约束1 确保路径从起点出发并在终点结束
 for i = 1:numrobot
-    C = [C, sum(x(i,l(i),:)) - x(i,l(i),l(i)) - sum(x(i,:,t(i))) + x(i,t(i),t(i))== 0];
+    C = [C, sum(x(l(i),:,i)) - x(l(i),l(i),i) - sum(x(:,t(i),i)) + x(t(i),t(i),i)== 0];
 
-    C = [C, sum(x(i,l(i),:)) - x(i,l(i),l(i)) == 1];
+    C = [C, sum(x(l(i),:,i)) - x(l(i),l(i),i) == 1];
 
-    C = [C, sum(x(i,:,l(i))) - x(i,l(i),l(i)) - sum(x(i,t(i),:)) + x(i,t(i),t(i)) == 0];
+    C = [C, sum(x(:,l(i),i)) - x(l(i),l(i),i) - sum(x(t(i),:,i)) + x(t(i),t(i),i) == 0];
 
-    C = [C, sum(x(i,:,l(i))) - x(i,l(i),l(i)) == 0]; 
+    C = [C, sum(x(:,l(i),i)) - x(l(i),l(i),i) == 0]; 
 end
 %% 约束2 确保出入边条件，每个顶点在路径中仅出现一次
 for k=1:numrobot
     for i = 1:n
         if i ~= l(k) && i~=t(k)
-            C = [C, sum(x(k,i,:))-x(k,i,i)- sum(x(k,:,i))+x(k,i,i) == 0];
-            C = [C, sum(x(k,i,:))-x(k,i,i) <= 1];
-            C = [C, sum(x(k,:,i))-x(k,i,i) <= 1];
+            C = [C, sum(x(i,:,k))-x(i,i,k)- sum(x(:,i,k))+x(i,i,k) == 0];
+            C = [C, sum(x(i,:,k))-x(i,i,k) <= 1];
+            C = [C, sum(x(:,i,k))-x(i,i,k) <= 1];
         end
     end
 end
@@ -85,7 +85,7 @@ for k=1:numrobot
     for i = 1:n
         for j = 1:n
             if i~=j && i ~=l(k) && i ~=t(k) && j ~=l(k) && j ~=t(k)
-                C = [C,u(k,i)-u(k,j) + (n-3)*x(k,i,j)<=n-4];
+                C = [C,u(k,i)-u(k,j) + (n-3)*x(i,j,k)<=n-4];
             end
         end
     end
@@ -99,14 +99,14 @@ for i = 1:m
     if mod(i,2)==0  && D(i_x,i_y) ~=1 
         if mod(i_x,2)==1 && mod(i_y,2)==0
 %             if i+1~=t
-                dir_rob(k,i/2) = x(k,i,i+1)+3*x(k,i+1,i);
+                dir_rob(k,i/2) = x(i,i+1,k)+3*x(i+1,i,k);
 %             elseif i+1 ==t
 %                 dir_rob(k,i/2) = x(k,i-1,i)+3*x(k,i,i-1);
 %             end
         end
         if mod(i_x,2)==0 && mod(i_y,2)==1
 %             if i+size_D~=t
-                dir_rob(k,i/2) = x(k,i,i+size_D)+3*x(k,i+size_D,i);
+                dir_rob(k,i/2) = x(i,i+size_D,k)+3*x(i+size_D,i,k);
 %             elseif i+size_D==t
 %                 dir_rob(k,i/2) = x(k,i-size_D,i)+3*x(k,i,i-size_D);
 %             end
@@ -236,7 +236,7 @@ for k=1:numrobot
     for i = 1:n
         for j = 1:n
             if i==j 
-                C = [C,x(k,i,j)==0];
+                C = [C,x(i,j,k)==0];
             end
         end
     end
@@ -247,6 +247,8 @@ end
 % 参数设置
 assign(dir_way,ini_dir_way);
 assign(dir_rob,ini_dir_rob);
+assign(x,ini_x_value);
+assign(u,ini_u_value);
 
 ops = sdpsettings('verbose',1,'solver','gurobi','usex0',1,'gurobi.TimeLimit',timelimit);%verbose计算冗余量，值越大显示越详细
 %ops = sdpsettings('verbose',0,'solver','cplex');
@@ -255,16 +257,29 @@ result  = optimize(C,z,ops);
 if result.problem== 0
 %    value(z)
 %     disp(value(dir_rob))
-%    text=' 系统总最优路径长度：';
-%    disp([text,num2str(value(z))]);
+%     text=' 系统总最优路径长度：';
+%     disp([text,num2str(value(z))]);
 %     disp('系统总最优路径长度：Best objective');
 else
     disp('Finish ! ');
-    disp(value(dir_rob))
+%     disp(value(dir_rob))
 end
 
 dir_way_value=value(dir_way);
 dir_rob_value=value(dir_rob);
+
+x_value = value(x);
+u_value = value(u);
+
+x_add=zeros(n,n);
+x_value=cat(3,x_value,x_add);
+
+u_add=zeros(1,n);
+u_value=cat(1,u_value,u_add);
+
+dir_rob_add=zeros(1,num_way);
+dir_rob_value=cat(1,dir_rob_value,dir_rob_add);
+
 
 %disp(value(dir))
 o=value(x);
@@ -272,7 +287,7 @@ o=value(x);
 %% 邻接矩阵转换，路径绘制
 
 for i = 1:numrobot
-    o_single{i,1} =  squeeze(o(i,:,:)) ; 
+    o_single{i,1} =  squeeze(o(:,:,i)) ; 
 end
 
 for i = 1:numrobot
--- main_test.m
+++ main_test.m
@@ -6,35 +6,38 @@ RobotNum_total=20;
 
 [Start_ori,Goal_ori]=rand_Goal_Start(D,RobotNum_total); % 随机生成一组测试集
 
-run_time=zeros(1,RobotNum_total-3);
+run_time=zeros(1,RobotNum_total);
 
 [ini_dir_way] = initial();
 
-for i = 4:RobotNum_total
-    disp(['执行次数：',num2str(i-3)])
+for i = 1:RobotNum_total
+    disp('   ');
+    disp(['执行次数：',num2str(i)])
     Start_test = Start_ori(1:i);
     Goal_test = Goal_ori(1:i);
-    if i == 4
+    if i == 1
     tic
 
-        [PathStore,Path_num,ini_dir_way,ini_dir_rob]=IP_solver_single_way_ini(D,Start_test,Goal_test,i,size_D,ini_dir_way);    % 将上一次求解所得ini_dir_way作为原始解输入
+        [PathStore,Path_num,ini_dir_way,ini_dir_rob,ini_x_value,ini_u_value]=IP_solver_single_way_ini(D,Start_test,Goal_test,i,size_D,ini_dir_way);    % 将上一次求解所得ini_dir_way作为原始解输入
 
     toc
     else
         tic
-         [PathStore,Path_num,ini_dir_way,ini_dir_rob]=IP_solver_single_way_test(D,Start_test,Goal_test,i,size_D,ini_dir_way,ini_dir_rob);
+         [PathStore,Path_num,ini_dir_way,ini_dir_rob,ini_x_value,ini_u_value]=IP_solver_single_way_test(D,Start_test,Goal_test,i,size_D,ini_dir_way,ini_dir_rob,ini_x_value,ini_u_value);
         toc
     end
-    run_time(i-3)=toc;
-    disp(['机器人序号：',i])
+    run_time(i)=toc;
+    disp(['执行机器人个数：',num2str(i)])
     disp(['运行时间: ',num2str(toc)])
 end
 
+plotdynamic(D,PathStore,Path_num,RobotNum_total,Start_ori,Goal_ori);
+
 save('run_time.mat')
 
-figure(1)
+figure(2)
 plot(run_time,'-o')
 hold on
-p = polyfit((1:RobotNum_total-3),run_time,1);
-y1=polyval(p,(1:RobotNum_total-3));
+p = polyfit((1:RobotNum_total),run_time,1);
+y1=polyval(p,(1:RobotNum_total));
 plot(y1)
